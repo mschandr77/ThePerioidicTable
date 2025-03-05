acac6791a222eb63c1899ed2b3b9e794bbd1d359
Marvish Subhas Chandra <marvishchandra@outlook.com>
2025-03-04 19:34:51 -0800

    Add files via upload

---
+++ Element.java
@@ -0,0 +1,14 @@
+//public class Element {
+//    int IUPACgroup = 1;
+//    String groupTitle = "Alkali Metals";
+//    int columnNumber = 1;
+//    int periodNumber = 1;
+//    int atomicNumber = 1;
+//    String atomicSymbol = "H";
+//    String elementName = "Hydrogen";
+//    double elementMass = 1.01;
+//    public String toString() {
+//        String Hydrogen = IUPACgroup + "\n" + groupTitle + "\n" + columnNumber+ "\n" + periodNumber + '\n' + atomicNumber + '\n' + atomicSymbol + '\n' + elementName + '\n' + elementMass + '\n';
+//        return Hydrogen;
+//    }
+//}
